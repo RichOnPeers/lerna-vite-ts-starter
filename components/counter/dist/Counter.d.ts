c5db147e13ec809896e5b878b26b5d944f69078c
Richard Persson <richard.persson@cybercom.com>
2022-12-16 16:35:15 +0100

    add typescript package

--- components/counter/dist/Counter.d.ts
+++
@@ -1,7 +0,0 @@
-import { Dispatch, SetStateAction } from "react";
-interface CounterProps {
-    count: number;
-    setCount: Dispatch<SetStateAction<number>>;
-}
-export declare function Counter({ count, setCount }: CounterProps): JSX.Element;
-export {};
--- components/counter/dist/Counter.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-export function Counter({ count, setCount }) {
-    return (React.createElement("p", null,
-        React.createElement("button", { onClick: () => setCount((count) => count + 1) },
-            "count is: ",
-            count)));
-}
