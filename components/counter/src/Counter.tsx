d8ae5ae0b5a5a52723c8a418a12530efb096b6ca
Richard Persson <richard.persson@cybercom.com>
2022-12-19 13:27:19 +0100

    fix: update ts naming

--- components/counter/src/Counter.tsx
+++ components/counter/src/Counter.tsx
@@ -1,16 +1,14 @@
-import React, { Dispatch, SetStateAction } from "react";
+import React, { Dispatch, SetStateAction } from 'react';
 
-interface CounterProps {
+interface ICounterProps {
   count: number;
   setCount: Dispatch<SetStateAction<number>>;
 }
 
-export function Counter({ count, setCount }: CounterProps) {
+export function Counter({ count, setCount }: ICounterProps): JSX.Element {
   return (
     <p>
-      <button onClick={() => setCount((count: number) => count + 1)}>
-        count is: {count}
-      </button>
+      <button onClick={() => setCount((count: number) => count + 1)}>count is: {count}</button>
     </p>
   );
 }
